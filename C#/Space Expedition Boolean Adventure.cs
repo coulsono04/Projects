16c0b1c4999dd449b28822b6d311524be4a8cf06
coulsono04 <coulsono04@gmail.com>
2026-04-23 14:34:17 +0100

    Add files via upload

---
+++ C#/Space Expedition Boolean Adventure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class SpaceExpedition
+{
+    public static void Main(string[] args)
+    {
+        // This Project tests skills with boolean logic and comparison operators to check if planets are habitable.
+
+        bool isAtmosphereBreathable = true;
+        bool isGravityStable = false;
+        bool resourcesSufficient = true;
+        bool isHabitable = (isAtmosphereBreathable && isGravityStable);
+        Console.WriteLine(isHabitable);
+        bool suitableForExpansion = (isGravityStable || resourcesSufficient);
+        Console.WriteLine(suitableForExpansion);
+        int currentCrew = 5;
+        int maxCrewCapacity = 10;
+        bool canAcceptMoreCrew = (currentCrew < maxCrewCapacity);
+        Console.WriteLine(canAcceptMoreCrew);
+    }
+}
