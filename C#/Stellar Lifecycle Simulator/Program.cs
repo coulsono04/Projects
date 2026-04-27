453a280171a5dd151b2b3fc4e80fbfe5b0a9f359
coulsono04 <coulsono04@gmail.com>
2026-04-27 16:57:06 +0100

    Add files via upload

---
+++ C#/Stellar Lifecycle Simulator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StarLifecycleSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This project assess skills usings classes/Abstraction, and works alongside the other file "Stars" which holds the class. This file runs the main program
+
+            /* Star Simulations */
+
+            Star sun = new Star("Sun", "G-Type");
+            sun.Shine();
+            sun.GrowOlder();
+            sun.Shine();
+            sun.Supernova();
+
+            Star unknownStar = new Star("Mystery Star");
+            unknownStar.Shine();
+            unknownStar.GrowOlder();
+        }
+    }
+}
