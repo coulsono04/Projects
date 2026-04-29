bbc7b79d4984de516518236a50e8fa52cb260a07
coulsono04 <coulsono04@gmail.com>
2026-04-29 10:33:37 +0100

    Add files via upload

---
+++ C#/The Object of Your Affection/Profile.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Profile
+    {
+        // This is where the class for the dating app project is created, with a focus on improving skills with encapsulation.
+
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name, int age, string city, string country, string[] hobbies, string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = hobbies ?? new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string mainString = this.name + ", " + this.age + ", " + this.city + ", " + this.country + ", " + this.pronouns + ", hobbies: ";
+            string result = string.Join(", ", this.hobbies);
+            return mainString + result;
+
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+
+    }
+}
---
+++ C#/The Object of Your Affection/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatingProfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This project practices skills using encapsulation by creating a dating profile service. This cs file handles creating the individuals profile object.
+
+            string[] hobs = { "listening to audiobooks and podcasts", "playing rec sports like bowling and kickball", "writing a speculative fiction novel", "reading advice columns" };
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", hobs, "he/him");
+            Console.WriteLine(sam.ViewProfile());
+
+        }
+    }
+}
